69caae65848b320bc88de735cf69d3030e4ebdb0
sveerapaneni <sveerapaneni@geninfo.com>
2014-08-09 12:33:21 -0400

    Initial Checkin

---
+++ Candies/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackerRankCore;
+
+namespace Candies
+{
+    class CandiesMain
+    {
+        static void Main(string[] args)
+        {
+            HackerRankCoreCls oHr = new HackerRankCoreCls();
+            try
+            {
+                ArrayList oAl = oHr.ReadFile4Ints("Input.txt");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ HackerRankMain/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankCore
+{
+    public class HackerRankCoreCls
+    {
+        public ArrayList ReadFile4Ints(string fPath)
+        {
+            ArrayList oAl = new ArrayList();
+            try
+            {
+                string sLine = "";
+                int iElem = 0;
+                int i = 0;
+                using (StreamReader oSr = new StreamReader(fPath))
+                {
+                    while ((sLine = oSr.ReadLine()) != null)
+                    {
+                        if (sLine.Trim() != "" && Int32.TryParse(sLine.Trim(), out iElem))
+                        {
+                            if (i >= 1)
+                                oAl.Add(iElem);
+                            i++;
+                        }
+                    }
+                    if (oAl.Count!=i-1)
+                        throw new Exception("Invalid input file!");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return oAl;
+
+        }
+    }
+}
